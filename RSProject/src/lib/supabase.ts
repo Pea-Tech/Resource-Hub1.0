df44038d57b89b3de41c4780d7a820a70ee7d810
Pea-Tech <156255412+Pea-Tech@users.noreply.github.com>
2024-10-29 12:06:06 +0100

    supabase.ts

--- RSProject/src/lib/supabase.ts
+++ RSProject/src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lnczylhudqyedannsmur.supabase.co';
+const supabaseUrl = 'Your_supabase_Url';
 const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuY3p5bGh1ZHF5ZWRhbm5zbXVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk4NTc2NTUsImV4cCI6MjA0NTQzMzY1NX0.85A5jKkNuOGF7gt4yG_KJIYG1FjhgYHdnLUJT5s0Lyg';
+  'Your_supabase_anon_key';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
